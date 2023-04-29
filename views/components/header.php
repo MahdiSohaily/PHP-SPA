7c3f52fecbb2f15e56e1d9359ac77a29b525d4d4
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-29 20:02:05 +0330

    Add hero page sections

---
+++ views/components/header.php
@@ -0,0 +1,15 @@
+<header class="page-header">
+  <div class="page-header__top">
+    <div class='page-header__logo'>LOGO</div>
+    <div class="page-header__toggle" onclick="toggleMenu()">☰</div>
+  </div>
+  <nav class="page-header__bottom">
+    <ul id="navigation" class="navigation">
+      <li class="navigation__item">Home</li>
+      <li class="navigation__item">About</li>
+      <li class="navigation__item">Portfolio</li>
+      <li class="navigation__item">Blog</li>
+      <li class="navigation__item">Contact</li>
+    </ul>
+  </nav>
+</header>
