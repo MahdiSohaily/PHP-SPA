11b3c1743a6ac5327b0663cbe5b4c56f32aeb8d9
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-29 17:32:00 +0330

    Add navigation

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,12 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Relation routes
 });
 
 require __DIR__.'/auth.php';
