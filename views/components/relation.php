8c90379c75393ecda0e68cc908dd05f8e2475a19
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-30 13:05:35 +0330

    Add function send axios requeat

--- views/components/relation.php
+++ views/components/relation.php
@@ -1,10 +1,10 @@
 <section class='relation'>
     <div class="section serial-form">
         <form class="center" action="#" method="post">
-            <input class="serial-input" type="text" name="serial" id="serial" placeholder="کد قطعه فنی را وارد کنید ...">
+            <input class="serial-input" type="text" name="serial" id="serial" placeholder="کد قطعه فنی را وارد کنید ..." onkeyup="search(this.value)">
         </form>
         <section id="match" style="direction: rtl;">
-            <div id="search-result" class="list-group">
+            <div id="s-result" class="list-group">
                 <div class="matched-item">
                     <p>کد فنی قطعه</p>
                     <i class='material-icons add'>add_circle_outline</i>
@@ -55,7 +55,7 @@
                 <p>کد فنی قطعه</p>
             </div>
             <div class="matched-item">
-            <i class='material-icons remove'>do_not_disturb_on</i>
+                <i class='material-icons remove'>do_not_disturb_on</i>
                 <p>کد فنی قطعه</p>
             </div>
         </section>
@@ -68,3 +68,25 @@
         </form>
     </div>
 </section>
+
+<script>
+    function search(val) {
+        let supermode = 0;
+        const resultBox = document.getElementById('s-result')
+
+
+        if (val.length > 6) {
+            resultBox.innerHTML =
+                "<img id='loading' src='<?php echo URL_ROOT . URL_SUBFOLDER ?>/public/img/loading.gif' alt=''>";
+            axios.get('getdata/' + val)
+                .then(response => {
+                    resultBox.innerHTML = response.data;
+                }).catch(error => {
+                    console.log(error);
+                })
+        } else {
+            resultBox.innerHTML =
+                "<img id='loading' src='<?php echo URL_ROOT . URL_SUBFOLDER ?>/public/img/loading.gif' alt=''>";
+        }
+    }
+</script>
