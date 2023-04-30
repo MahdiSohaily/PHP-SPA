3561db936ae11f9b9b0afe518663a73806423e78
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-30 11:25:32 +0330

    Add styles for serial form

--- views/components/relation.php
+++ views/components/relation.php
@@ -1,3 +1,17 @@
-<div>
-    <h1>Relation page</h1>
-</div>
+<section class='relation'>
+    <div class="serial-form">
+        <form action="#" method="post">
+            <input class="serial-input" type="text" name="serial" id="serial" placeholder="کد قطعه فنی را وارد کنید ...">
+        </form>
+        <ul id="match">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+        </ul>
+    </div>
+    <div class="">2</div>
+    <div class="">3</div>
+</section>
