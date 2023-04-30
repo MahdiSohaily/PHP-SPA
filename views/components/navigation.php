9761968cdf780973fbd8ed342418b0455949d90d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-30 10:53:24 +0330

    Update navbar structure

--- views/components/navigation.php
+++ views/components/navigation.php
@@ -1,15 +1,29 @@
-<header class="page-header">
-  <div class="page-header__top">
-    <div class='page-header__logo'>LOGO</div>
-    <div class="page-header__toggle" onclick="toggleMenu()">☰</div>
-  </div>
-  <nav class="page-header__bottom">
-    <ul id="navigation" class="navigation">
-      <li class="navigation__item">Home</li>
-      <li class="navigation__item">About</li>
-      <li class="navigation__item">Portfolio</li>
-      <li class="navigation__item">Blog</li>
-      <li class="navigation__item">Contact</li>
-    </ul>
-  </nav>
-</header>
+<nav class="navbar navbar-expand-custom navbar-mainbg">
+        <a class="navbar-brand navbar-logo" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars text-white"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-address-book"></i>Address Book</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-clone"></i>Components</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-calendar-alt"></i>Calendar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-chart-bar"></i>Charts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-copy"></i>Documents</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
