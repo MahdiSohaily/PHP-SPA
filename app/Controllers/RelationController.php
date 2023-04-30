3e790ac12191363d792c90dfd2e68e4e45afea6c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-30 13:15:02 +0330

    Add search function for search controller

--- app/Controllers/RelationController.php
+++ app/Controllers/RelationController.php
@@ -2,14 +2,31 @@
 
 namespace App\Controllers;
 
+use App\Models\Good;
 use Symfony\Component\Routing\RouteCollection;
 
 class RelationController
 {
+	
     // Homepage action
 	public function index(RouteCollection $routes)
 	{
 		require_once APP_ROOT . '/views/components/app-templete.php';
 		
+	}
+	
+	public function search($pattern ,RouteCollection $routes)
+	{
+		if(isset($_COOKIE['login-user'])) {
+			$good = new Good();
+			$rate = new Rate();
+
+			$rates = $rate->getRates();
+
+			$result = $good->search($key, $mode, $rates);
+		} else {
+			header('Location: /yadak');
+			exit;
+		}
 	}
 }
