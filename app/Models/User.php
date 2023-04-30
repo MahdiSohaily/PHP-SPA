c714c2b2ed7643f36e36dd4b8c3f207c43cd7a94
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-30 11:54:52 +0330

    update structure

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace App\Models;
 
 class User
@@ -6,28 +7,28 @@ class User
 
     public function login($email, $password)
     {
-        $user = $this->checkUser($email,$password);
-        if($user) {
-			return $user;
+        $user = $this->checkUser($email, $password);
+        if ($user) {
+            return $user;
         } else {
             return false;
         }
     }
-	
-	public function checkUser($email, $pass)
-	{
+
+    public function checkUser($email, $pass)
+    {
         // Create connection
-        $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS,DB_NAME);
+        $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
         $sql = "SELECT * FROM users WHERE email='$email'";
-		$user = $conn->query($sql)->fetch_assoc();
-		$password = $user['password'];
-		$conn -> close();
+        $user = $conn->query($sql)->fetch_assoc();
+        $password = $user['password'];
+        $conn->close();
 
-        if (count($user)> 0 && $password === $pass) {
-			return $user;
-          } else {
+        if (count($user) > 0 && $password === $pass) {
+            return $user;
+        } else {
             return false;
-          }
-	}
+        }
+    }
 }
