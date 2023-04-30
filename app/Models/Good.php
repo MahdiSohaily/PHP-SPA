6e46a8ee301019fa9386cb6a2fe8c6abce36a7a0
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-30 13:28:05 +0330

    Update returned data template

--- app/Models/Good.php
+++ app/Models/Good.php
@@ -8,24 +8,29 @@ class Good
     {
         $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-
         $sql = "SELECT * FROM nisha WHERE partnumber LIKE '" . $pattern . "%'";
-
         $result = $conn->query($sql);
+        $templete = '';
 
         if ($result->num_rows > 0) {
             // output data of each row
             while ($row = $result->fetch_assoc()) {
+                $id = $row['id'];
                 $partnumber = $row['partnumber'];
                 $price = $row['price'];
                 $mobis = $row['mobis'];
+
+                $templete .= "<div class='matched-item'>
+                    <p>$partnumber</p>
+                    <p>$price</p>
+                    <p>$mobis</p>
+                    <i data-id='" . $id . "' class='material-icons add'>add_circle_outline</i>
+                </div>";
             }
         } else {
-            echo '<tr id="error">
-                <td colspan="15 fa">کد فنی اشتباه یا ناقص می باشد</td>
-            </tr>';
+            $templete = "<div class='matched-item'> <p>کد مشابه برای الگوی وارد شده دریافت نشد.</p> </div>";
         }
-
         $conn->close();
+        return $templete;
     }
 }
