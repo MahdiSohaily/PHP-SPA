adf7b712a02a3bf674f075ace9dca6b2f190d3f7
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-30 12:26:41 +0330

    Add styles to the element

--- views/components/relation.php
+++ views/components/relation.php
@@ -1,26 +1,35 @@
 <section class='relation'>
-    <div class="serial-form">
-        <form action="#" method="post">
+    <div class="section serial-form">
+        <form class="center" action="#" method="post">
             <input class="serial-input" type="text" name="serial" id="serial" placeholder="کد قطعه فنی را وارد کنید ...">
         </form>
-        <section id="match">
-            <div class="">1</div>
-            <div class="">2</div>
-            <div class="">3</div>
-            <div class="">4</div>
-            <div class="">5</div>
-            <div class="">6</div>
+        <section id="match" style="direction: rtl;">
+            <h2>اقلام مشابه</h2>
+            <div id="search-result" class="list group">
+                <div class="matched-item">1</div>
+                <div class="matched-item">2</div>
+                <div class="matched-item">3</div>
+                <div class="matched-item">4</div>
+                <div class="matched-item">5</div>
+                <div class="matched-item">6</div>
+            </div>
         </section>
     </div>
-    <div class="">
+    <div class="section">
         <section id="selected">
-            <div class="">1</div>
-            <div class="">2</div>
-            <div class="">3</div>
-            <div class="">4</div>
-            <div class="">5</div>
-            <div class="">6</div>
+            <div class="matched-item">1</div>
+            <div class="matched-item">2</div>
+            <div class="matched-item">3</div>
+            <div class="matched-item">4</div>
+            <div class="matched-item">5</div>
+            <div class="matched-item">6</div>
         </section>
     </div>
-    <div class="">3</div>
+    <div class="section">
+        <form action="#" method="post">
+            <input type="text" name="name" id="name" required>
+            <input type="text" name="car" id="car" required>
+            <input type="text" name="state" id="state" required>
+        </form>
+    </div>
 </section>
