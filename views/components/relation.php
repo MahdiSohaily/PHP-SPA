65330e88882cffb5621a1925c91cd01edf773977
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-30 12:53:46 +0330

    Add style for searched items

--- views/components/relation.php
+++ views/components/relation.php
@@ -4,25 +4,60 @@
             <input class="serial-input" type="text" name="serial" id="serial" placeholder="کد قطعه فنی را وارد کنید ...">
         </form>
         <section id="match" style="direction: rtl;">
-            <h2>اقلام مشابه</h2>
             <div id="search-result" class="list-group">
-                <div class="matched-item">1</div>
-                <div class="matched-item">2</div>
-                <div class="matched-item">3</div>
-                <div class="matched-item">4</div>
-                <div class="matched-item">5</div>
-                <div class="matched-item">6</div>
+                <div class="matched-item">
+                    <p>کد فنی قطعه</p>
+                    <i class='material-icons add'>add_circle_outline</i>
+                </div>
+                <div class="matched-item">
+                    <p>کد فنی قطعه</p>
+                    <i class='material-icons add'>add_circle_outline</i>
+                </div>
+                <div class="matched-item">
+                    <p>کد فنی قطعه</p>
+                    <i class='material-icons add'>add_circle_outline</i>
+                </div>
+                <div class="matched-item">
+                    <p>کد فنی قطعه</p>
+                    <i class='material-icons add'>add_circle_outline</i>
+                </div>
+                <div class="matched-item">
+                    <p>کد فنی قطعه</p>
+                    <i class='material-icons add'>add_circle_outline</i>
+                </div>
+                <div class="matched-item">
+                    <p>کد فنی قطعه</p>
+                    <i class='material-icons add'>add_circle_outline</i>
+                </div>
             </div>
         </section>
     </div>
     <div class="section">
         <section id="selected">
-            <div class="matched-item">1</div>
-            <div class="matched-item">2</div>
-            <div class="matched-item">3</div>
-            <div class="matched-item">4</div>
-            <div class="matched-item">5</div>
-            <div class="matched-item">6</div>
+            <div class="matched-item">
+                <i class='material-icons remove'>do_not_disturb_on</i>
+                <p>کد فنی قطعه</p>
+            </div>
+            <div class="matched-item">
+                <i class='material-icons remove'>do_not_disturb_on</i>
+                <p>کد فنی قطعه</p>
+            </div>
+            <div class="matched-item">
+                <i class='material-icons remove'>do_not_disturb_on</i>
+                <p>کد فنی قطعه</p>
+            </div>
+            <div class="matched-item">
+                <i class='material-icons remove'>do_not_disturb_on</i>
+                <p>کد فنی قطعه</p>
+            </div>
+            <div class="matched-item">
+                <i class='material-icons remove'>do_not_disturb_on</i>
+                <p>کد فنی قطعه</p>
+            </div>
+            <div class="matched-item">
+            <i class='material-icons remove'>do_not_disturb_on</i>
+                <p>کد فنی قطعه</p>
+            </div>
         </section>
     </div>
     <div class="section">
