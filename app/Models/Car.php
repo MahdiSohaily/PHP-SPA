6397d7018dfbf71dbfd226697cdd0ede81ad58cd
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-02 10:29:46 +0330

    Add functin to get searched car

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -17,4 +17,18 @@ class Car
         }
         return $product_array;
     }
+    
+    public function search($pattern)
+    {
+        $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+        $sql = "SELECT * FROM cars WHERE name LIKE '%$pattern%'";
+
+        $result = mysqli_query($conn, $sql);
+
+        while ($r = mysqli_fetch_assoc( $result)){
+            $product_array[] = $r;
+        }
+        return $product_array;
+    }
 }
