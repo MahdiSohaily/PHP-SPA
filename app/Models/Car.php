4c3748f8be39bdaa56085774be46902ec9dd3c46
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-02 09:34:24 +0330

    Update sql return value

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -9,7 +9,12 @@ class Car
         $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
         $sql = "SELECT * FROM cars";
-        $result = $conn->query($sql)->fetch_all();
-        return $result;
+
+        $result = mysqli_query($conn, $sql);
+
+        while ($r = mysqli_fetch_assoc( $result)){
+            $product_array[] = $r;
+        }
+        return $product_array;
     }
 }
