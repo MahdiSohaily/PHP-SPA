e3b9a830a6743376f6389a0ffd168ba97817e42f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-06 11:09:20 +0330

    Add script to check if the pattern is in the relation

--- app/Models/Good.php
+++ app/Models/Good.php
@@ -10,7 +10,30 @@ class Good
 
         $sql = "SELECT * FROM nisha WHERE partnumber LIKE '" . $pattern . "%'";
         $result = $conn->query($sql);
-        $templete = '';
+
+
+        $pattern = "SELECT id FROM patterns WHERE serial LIKE '" . $pattern . "%'";
+        $pattern_result = $conn->query($pattern);
+        $pattern_ids = [];
+
+        if ($pattern_result->num_rows > 0) {
+            while ($row = $pattern_result->fetch_assoc()) {
+                array_push($pattern_ids, $row['id']);
+            }
+        }
+
+        $similar = "SELECT nisha_id FROM similars WHERE pattern_id IN (1,2)";
+        $similar_result = $conn->query($similar);
+        $similar_ids = [];
+
+        if ($similar_result->num_rows > 0) {
+            while ($row = $similar_result->fetch_assoc()) {
+                array_push($similar_ids, $row['nisha_id']);
+            }
+        }
+
+
+        $template = '';
 
         if ($result->num_rows > 0) {
             // output data of each row
@@ -20,7 +43,19 @@ class Good
                 $price = $row['price'];
                 $mobis = $row['mobis'];
 
-                $templete .= "<div class='matched-item' id='$id'>
+                if (in_array($id, $similar_ids)) {
+                    $template .= "<div class='matched-item' id='$id'>
+                    <i onclick='add(event)'  
+                    data-id='" . $id . "' 
+                    data-partnumber='" . $partnumber . "' 
+                    data-price='" . $price . "' 
+                    data-mobis='" . $mobis . "' 
+                    class='material-icons add'>add_circle_outline</i>
+                    <p>$partnumber</p>
+                    <p>in relation</p>
+                </div>";
+                } else {
+                    $template .= "<div class='matched-item' id='$id'>
                     <i onclick='add(event)'  
                     data-id='" . $id . "' 
                     data-partnumber='" . $partnumber . "' 
@@ -29,11 +64,12 @@ class Good
                     class='material-icons add'>add_circle_outline</i>
                     <p>$partnumber</p>
                 </div>";
+                }
             }
         } else {
-            $templete = "<div class='matched-item'> <p>کد مشابه برای الگوی وارد شده دریافت نشد.</p> </div>";
+            $template = "<div class='matched-item'> <p>کد مشابه برای الگوی وارد شده دریافت نشد.</p> </div>";
         }
         $conn->close();
-        return $templete;
+        return $template;
     }
 }
