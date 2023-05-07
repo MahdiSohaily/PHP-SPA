b9582d442735fcf486ad70fbe8311d716be49b22
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-07 09:22:10 +0330

    Add main content part to the page

--- views/components/navigation.php
+++ views/components/navigation.php
@@ -45,13 +45,46 @@
         </ul>
     </nav>
     <main>
-        <?php
-        if (isset($body)) {
-            require_once $body;
-        } else {
-            require_once 'relation.php';
-        }
-        ?>
+        <section class='relation' style="direction: rtl;">
+            <div class="section serial-form">
+                <form class="center" method="post">
+                    <input class="serial-input" type="text" name="serial" id="serial" placeholder="کد قطعه فنی را وارد کنید ..." onkeyup="search(this.value)">
+                </form>
+                <section id="match" style="direction: rtl;">
+                    <div id="s-result" class="list-group">
+                        <!-- The searched data is going to be added here -->
+                    </div>
+                </section>
+            </div>
+            <div class="section">
+                <section id="selected" style="direction: rtl;">
+                    <h2>موارد انتخاب شده:</h2>
+                    <!-- selected items are going to be appended here -->
+                </section>
+            </div>
+            <div class="section">
+                <form class='add-relation' action="#" method="post" onsubmit="event.preventDefault(); send()">
+                    <input class="r-input" type="text" name="mode" value="create" hidden required>
+
+                    <input class="r-input" placeholder="نام" type="text" name="name" id="name" required>
+                    <select id="car_id" class="r-input" name="car_id" required>
+                        <?php
+                        if ($cars) {
+                            foreach ($cars as $car) {
+                                echo '<option id="c-' . $car['id'] . '" value="' . $car['id'] . '">' . $car['name'] . '</option>';
+                            }
+                        }
+                        ?>
+                    </select>
+                    <select name="status" id="status" class="r-input" required>
+                        <option value="1">نو</option>
+                        <option value="2">در حد نو</option>
+                        <option value="5">کارکرده</option>
+                    </select>
+                    <input class="r-input bg-green" value="ثبت" type="submit" name="submit">
+                </form>
+            </div>
+        </section>
     </main>
 </body>
 
