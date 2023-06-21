43f7a139a2de6b909df50e8c330e2179f0a48c80
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-21 11:42:23 +0330

    Add new configuration

--- relationshops.php
+++ relationshops.php
@@ -2,8 +2,8 @@
 require_once './config/config.php';
 require_once './database/connect.php';
 require_once('./views/Layouts/header.php');
-$sql = "SELECT * FROM rates ORDER BY amount ASC";
-$rates = $conn->query($sql);
+$sql = "SELECT * FROM cars";
+$cars = $conn->query($sql);
 ?>
 <div class="rtl h-70S grid grid-cols-1 my-8 md:grid-cols-3 gap-6 lg:gap-8 p-6 lg:p-8">
     <div class="bg-white rounded-lg shadow-md">
@@ -63,104 +63,60 @@ $rates = $conn->query($sql);
                 <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md">
                     <div class="grid grid-cols-6 gap-6">
                         <input type="text" name="form" value="update" hidden>
-                        <div class="col-span-6 sm:col-span-4">
+                        <div class="col-span-12 sm:col-span-4">
                             <label class="block font-medium text-sm text-gray-700">
-                                شماره فنی
+                                اسم رابطه
                             </label>
-                            <input name="partNumber" value="<?php echo $selected_good['partnumber'] ?>" class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm px-3 py-2" required id="serial" type="text" />
+                            <input name="relation_name" value="<?php echo $selected_good['partnumber'] ?>" class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm px-3 py-2" required id="serial" type="text" />
                             <p class="mt-2"></p>
                         </div>
-
-                        <!-- Price -->
-                        <div class="col-span-6 sm:col-span-4">
+                        <div class="col-span-12 sm:col-span-4">
                             <label class="block font-medium text-sm text-gray-700">
-                                قیمت جنس
+                                قیمت
                             </label>
-                            <input name="price" value="<?php echo $selected_good['price'] ?>" class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm px-3 py-2" id="price" type="text" />
-                            <p class="mt-2"> </p>
+                            <input name="price" value="<?php echo $selected_good['partnumber'] ?>" class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm px-3 py-2" id="serial" type="text" />
+                            <p class="mt-2"></p>
                         </div>
-                        <!-- Weight -->
-                        <div class="col-span-6 sm:col-span-4">
-                            <label class="block font-medium text-sm text-gray-700">
-                                وزن جنس
+                        <div class="col-span-12 sm:col-span-4">
+                            <label for="cars">
+                                خودرو های مرتبط
                             </label>
-                            <input name="weight" value="<?php echo $selected_good['weight'] ?>" class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm px-3 py-2" id="weight" type="text" />
-                            <p class="mt-2"> </p>
+                            <select type="cars" multiple class="mt-1 block w-full border-gray-300 ltr focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" autocomplete="username" v-model="form.car_id" id="cars">
+                                <?php
+                                if (mysqli_num_rows($cars) > 0) {
+                                    while ($item = mysqli_fetch_assoc($cars)) {
+                                ?>
+                                        <option v-for="item in cars" value="<?php echo $item['id'] ?>" class="text-sm">
+                                            <?php echo $item['name'] ?>
+                                        </option>
+                            </select>
+                    <?php }
+                                } ?>
                         </div>
-                        <!-- Mobis -->
-                        <div class="col-span-6 sm:col-span-4">
-                            <label class="block font-medium text-sm text-gray-700">
-                                موبیز
+                        <div class="col-span-12 sm:col-span-4">
+                            <label for="cars">
+                                وضعیت
                             </label>
-                            <input name="mobis" value="<?php echo $selected_good['mobis'] ?>" class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm px-3 py-2" id="mobis" type="text" />
-                            <p class="mt-2"> </p>
-                        </div>
-                        <!-- Korea section -->
-                        <div class="col-span-6 sm:col-span-4">
-                            <label class="block font-medium text-sm text-gray-700">
-                                کوریا
-                            </label>
-                            <input name="korea" value="<?php echo $selected_good['korea'] ?>" class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm px-3 py-2" id="korea" v-model="form.korea" type="text" />
-                            <p class="mt-2"> </p>
+                            <select type="status" class="mt-1 block w-full ltr border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" autocomplete="username" v-model="form.status_id" id="status">
+                                <option v-for="item in status" :value="item.id" class="text-sm">
+                                    {{ item.name }}
+                                </option>
+                            </select>
                         </div>
                     </div>
                 </div>
 
-                <div v-if="hasActions" class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+                <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
                     <button type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                         <i class="px-2 material-icons hover:cursor-pointer">save</i>
                         ذخیره سازی
                     </button>
-                    <?php
-                    if ($success) {
-                    ?>
-                        <br>
-                        <p class="text-green-400"><?php echo $success ?></p>
-                    <?php
-                    }
-                    ?>
-
-                    <?php
-                    if ($errors) {
-                    ?>
-                        <br>
-                        <p class="text-orange-400"><?php echo $errors ?></p>
-                    <?php
-                    }
-                    ?>
                 </div>
             </form>
             <FormRelation @submitted="createRelation">
                 <template #form>
-                    <!-- Name -->
-                    <div class="pb-2">
-                        <InputLabel for="name" value=" />
-                        <TextInput id="name" v-model="form.name" type="text" class="mt-1 block w-full ltr" autocomplete="name" />
-                        <InputError :message="form.errors.name" class="mt-2" />
-                    </div>
-                    <div class="pb-2">
-                        <InputLabel for="price" value="قیمت" />
-                        <TextInput id="price" v-model="form.price" type="text" class="mt-1 block w-full ltr" autocomplete="price" />
-                        <InputError :message="form.errors.price" class="mt-2" />
-                    </div>
-                    <div class="pb-2">
-                        <InputLabel for="cars" value="خودرو های مرتبط" />
-                        <select type="cars" multiple class="mt-1 block w-full border-gray-300 ltr focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" autocomplete="username" v-model="form.car_id" id="cars">
-                            <option v-for="item in cars" :value="item.id" class="text-sm">
-                                {{ item.name }}
-                            </option>
-                        </select>
-                        <InputError :message="form.errors.car_id" class="mt-2" />
-                    </div>
-                    <div class="pb-2">
-                        <InputLabel for="status" value="وضعیت" />
-                        <select type="status" class="mt-1 block w-full ltr border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" autocomplete="username" v-model="form.status_id" id="status">
-                            <option v-for="item in status" :value="item.id" class="text-sm">
-                                {{ item.name }}
-                            </option>
-                        </select>
-                        <InputError :message="form.errors.status_id" class="mt-2" />
-                    </div>
+
+
                     <InputError :message="form.errors.values" class="mt-2" />
                 </template>
 
