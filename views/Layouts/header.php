37163b023dba70f8a0668bb0ff6df622c470aa0d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-21 15:48:32 +0330

    Add new configuration

--- views/Layouts/header.php
+++ views/Layouts/header.php
@@ -48,7 +48,7 @@ require_once './database/connect.php';
                         <i class="px-2 material-icons hover:cursor-pointer">show_chart</i>
                         نرخ های ارز
                     </a>
-                    <a class="cursor-pointer inline-flex items-center py-3 pr-6 text-sm font-medium leading-5 text-gray-500 hover:bg-indigo-500 hover:text-white focus:outline-none transition duration-150 ease-in-out" href="../../relationshops.php">
+                    <a class="cursor-pointer inline-flex items-center py-3 pr-6 text-sm font-medium leading-5 text-gray-500 hover:bg-indigo-500 hover:text-white focus:outline-none transition duration-150 ease-in-out" href="../../relationships.php">
                         <i class="px-2 material-icons hover:cursor-pointer">sync</i>
                         تعریف رابطه اجناس
                     </a>
