00594dca56a2d0d15ecb8d82fc837fe6fa40046f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-21 14:20:25 +0330

    Add new configuration

--- relationshops.php
+++ relationshops.php
@@ -129,6 +129,9 @@ $status = $conn->query($status_sql);
                     </select>
                 </div>
         </div>
+        <p id="select_box_error" class="px-3 tiny-text text-green-500 hidden">
+            رابطه جدید اجناس موفقانه در پایگاه داده ثبت شد!
+        </p>
 
         <div class="flex items-center justify-end px-4 py-3  text-right sm:px-6">
             <button type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
