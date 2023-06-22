27ba9054e77a440a258a28a0788c779fc1381cd5
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-22 18:27:15 +0330

    Add the given price controller

--- app/Controllers/GivenPriceController.php
+++ app/Controllers/GivenPriceController.php
@@ -1,7 +1,15 @@
 <?php
+$isValidCustomer = false;
+$finalResult = null;
+
 if (isset($_POST['givenPrice'])) {
     $customer = $_POST['customer'];
     $code = $_POST['code'];
 
-    echo $customer, $code;
+    $customer_sql = "SELECT * FROM callcenter.customer WHERE id = '" . $customer . "%'";
+    $result = mysqli_query($conn, $customer_sql);
+    if (mysqli_num_rows($result) > 0) {
+
+        $isValidCustomer = true;
+    }
 }
--- showPriceReports.php
+++ showPriceReports.php
@@ -3,4 +3,9 @@ require_once './database/connect.php';
 require_once('./views/Layouts/header.php');
 require_once('./app/Controllers/GivenPriceController.php');
 
+if (!$isValidCustomer) {
+} else {
+    echo 'Customer is not valid';
+}
+
 require_once('./views/Layouts/footer.php');
