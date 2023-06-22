51c95af86e87939dac14f0df67037ec25558fb68
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-22 14:41:56 +0330

    Add given price page

--- givePrice.php
+++ givePrice.php
@@ -9,20 +9,20 @@ $rates = $conn->query($sql);
 
     <form action="showPriceReports.php" method="post">
         <div class="">
-            <input type="text" name="form" value="update" hidden>
             <div class="col-span-6 sm:col-span-4">
                 <label for="customer" class="block font-medium text-sm text-gray-700">
                     مشتری
                 </label>
                 <input name="customer" class="border-1 border-gray-300 mt-1 block w-full border-gray-300 focus:border-indigo-500 p-3
-                focus:ring-indigo-500 rounded-md shadow-sm px-3" required id="customer" type="text" />
+                focus:ring-indigo-500 rounded-md shadow-sm px-3" required id="customer" type="number" />
                 <p class="mt-2"></p>
             </div>
             <!-- Korea section -->
             <div class="col-span-6 sm:col-span-4">
-                <InputLabel for="code" value="کدهای مدنظر" />
-                <textarea rows="7" id="code" v-model="form.code" type="text" required class="border-1 border-gray-300 ltr mt-1 shadow-sm block w-full rounded-md border-gray-300 p-3" placeholder="لطفا کد های مود نظر خود را در خط های مجزا قرار دهید"></textarea>
-                <InputError :message="form.errors.code" class="mt-2" />
+                <label for="code" class="block font-medium text-sm text-gray-700">
+                    کدهای مدنظر
+                </label>
+                <textarea rows="7" id="code" name="code" required class="border-1 border-gray-300 ltr mt-1 shadow-sm block w-full rounded-md border-gray-300 p-3" placeholder="لطفا کد های مود نظر خود را در خط های مجزا قرار دهید"></textarea>
             </div>
         </div>
 
