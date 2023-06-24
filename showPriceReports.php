26fbc99738aeadb12813294a2520abd8f8c8566e
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-24 13:14:47 +0330

    Add get estelam functionality

--- showPriceReports.php
+++ showPriceReports.php
@@ -12,6 +12,7 @@ if ($isValidCustomer) {
         $completeCode = $finalResult['completeCode'];
         $notification = $finalResult['notification'];
         $rates = $finalResult['rates'];
+
 ?>
         <div class="accordion mt-12">
             <?php
@@ -26,12 +27,15 @@ if ($isValidCustomer) {
                     $code = $code;
                     $existing_code = $existing[$code];
 
-                    foreach ($existing[$code] as $index => $item) {
-                        $information = $existing[$code][$index]['information'];
+                    foreach ($existing_code as $index => $item) {
                         $partNumber = $index;
-                        //$existing[$code][$index]['relation']['goods'] = $existing[$code][$index]['relation']['goods'];
-                        $exist = $existing[$code][$index]['relation']['existing'];
-                        $givenPrice = array_key_exists("givenPrice", $existing[$code][$index]) ? $existing[$code][$index]['givenPrice'] : null;
+                        $information = $item['information'];
+                        $relation = $item['relation'];
+                        $exist =  $relation['existing'];
+                        $givenPrice = array_key_exists("givenPrice", $item) ? $item['givenPrice'] : [];
+                        $customer = $customer;
+                        $completeCode = $completeCode;
+
                     ?>
                         <div class="grid grid-cols-1 md:grid-cols-10 gap-6 lg:gap-2 lg:p-2">
 
@@ -61,144 +65,6 @@ if ($isValidCustomer) {
                                 </div>
                             </div>
                             <!-- ENd the code info section -->
-
-
-                            <!-- Start of relations Details section -->
-                            <div class="bg-white rounded-lg col-span-5 overflow-auto">
-                                <div id="search_result" class="p-3">
-                                    <table class="min-w-full text-left text-sm font-light custom-table">
-                                        <thead class="font-medium bg-green-600">
-                                            <tr>
-                                                <th scope="col" class="px-3 py-3 text-white text-center">
-                                                    شماره فنی
-                                                </th>
-                                                <th scope="col" class="px-3 py-3 text-white text-center">
-                                                    مقدار موجودی
-                                                </th>
-                                                <th scope="col" class="px-3 py-3 text-white text-center">
-                                                    قیمت به اساس نرخ ارز
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            foreach ($existing[$code][$index]['relation']['sorted'] as $key => $element) {
-
-                                            ?>
-                                                <tr class="relative" v-for="element, key of props.relation.sorted">
-                                                    <td class=" px-1">
-                                                        <p class="text-center bold bg-gray-600 text-white px-2 py-3">
-                                                            <?php echo$existing[$code][$index]['relation']['goods'][$key]['partnumber'] ?>
-                                                        </p>
-
-                                                    </td>
-                                                    <td class="px-1 pt-2">
-                                                        <table class="min-w-full text-sm font-light p-2">
-                                                            <thead class="font-medium">
-                                                                <tr>
-                                                                    <?php
-                                                                    foreach ($existing[$code][$index]['relation']['existing'][$key] as $index => $goodAmount) {
-                                                                    ?>
-                                                                        <th scope="col" class="<?php echo $index == 'GEN' || $index == 'MOB' ? $index : 'brand-default' ?> text-white text-center py-2 relative hover:cursor-pointer" data-key="<?php echo $key ?>" data-brand="<?php echo $index ?>" onmouseover="seekExist" onmouseleave="closeSeekExist">
-                                                                            <?php echo $index ?>
-                                                                            <div class="custome-tooltip" id="<?php echo $key + '-' + $index ?>">
-                                                                                <?php
-                                                                                foreach ($existing[$code][$index]['relation']['goods']['stockInfo'][$key][$index] as $iterator => $item) {
-                                                                                ?>
-                                                                                    <div>
-                                                                                        <p v-if="item > 0"> <?php echo $iterator . ':' . $item ?></p>
-                                                                                    </div>
-                                                                                <?php } ?>
-                                                                            </div>
-                                                                        </th>
-                                                                    <?php } ?>
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                                <tr class="py-3">
-                                                                    <?php foreach ($existing[$code][$index]['relation']['existing'][$key] as $index => $goodAmount) { ?>
-                                                                        <td class="<?php echo $index == 'GEN' || $index == 'MOB' ? $index : 'brand-default' ?> whitespace-nowrap text-white px-3 py-2 text-center">
-                                                                            <?php echo $goodAmount ?>
-                                                                        </td>
-                                                                    <?php } ?>
-                                                                </tr>
-                                                            </tbody>
-                                                        </table>
-                                                    </td>
-                                                    <td class="px-1 pt-2">
-                                                        <table class="min-w-full text-left text-sm font-light">
-                                                            <thead class="font-medium">
-                                                                <tr>
-                                                                    <?php
-                                                                    foreach ($rates as $rate) {
-                                                                    ?>
-                                                                        <th v-for="rate in rates" scope="col" class="text-gray-800 text-center py-2 <?php echo $rate['status'] !== 'N' ? $rate['status'] : 'bg-green-700' ?>">
-                                                                            <?php echo $rate['amount'] ?>
-                                                                        </th>
-                                                                    <?php } ?>
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                                <tr class="py-3">
-                                                                    <?php
-                                                                    foreach ($rates as $rate) {
-                                                                    ?>
-                                                                        <td class="text-bold whitespace-nowrap px-3 py-2 text-center hover:cursor-pointer <?php echo $rate['status'] !== 'N' ? $rate['status'] : 'bg-gray-100' ?>" v-for="rate in rates" @click="$emit('setPrice', calculateRegular(props.relation.goods[key].price, rate.amount))">
-                                                                            <?php echo round(
-                                                                                round(($existing[$code][$index]['relation']['goods']['goods'][$key]['price'] * 110) / 243.5) *
-                                                                                    $rate['amount'] *
-                                                                                    1.2 *
-                                                                                    1.2 *
-                                                                                    1.3
-                                                                            ) ?>
-                                                                        </td>
-                                                                    <?php } ?>
-                                                                </tr>
-                                                                <?php
-                                                                if ($existing[$code][$index]['relation'][$key]['mobis'] > 0 && $existing[$code][$index]['relation']['goods']['goods'][$key]['mobis'] !== '-') {
-                                                                ?>
-                                                                    <tr class="bg-neutral-400" v-if="">
-                                                                        <?php
-                                                                        foreach ($rates as $rate) {
-                                                                        ?>
-                                                                            <td class="text-bold whitespace-nowrap px-3 text-center py-2 hover:cursor-pointer" @click="$emit('setPrice', calculateMobies(props.relation.goods[key].price, rate.amount))">
-                                                                                <?php echo round(
-                                                                                    round(($existing[$code][$index]['relation']['goods']['goods'][$key]['price'] * 110) / 243.5) *
-                                                                                        $rate['amount'] *
-                                                                                        1.25 *
-                                                                                        1.3
-                                                                                ) ?>
-
-                                                                            </td>
-                                                                        <?php } ?>
-                                                                    </tr>
-                                                                <?php }
-
-                                                                if ($existing[$code][$index]['relation']['goods']['goods'][$key]['korea'] > 0) {
-                                                                ?>
-                                                                    <tr class="bg-amber-600">
-                                                                        <?php
-                                                                        foreach ($rates as $rate) {
-                                                                        ?>
-                                                                            <td class="text-bold whitespace-nowrap px-3 text-center py-2 hover:cursor-pointer" @click="$emit('setPrice', calculateMobies(props.relation.goods[key].price, rate.amount))">
-                                                                                <?php echo round(
-                                                                                    round(($existing[$code][$index]['relation']['goods']['goods'][$key]['price'] * 110) / 243.5) *
-                                                                                        $rate['amount'] *
-                                                                                        1.25 *
-                                                                                        1.3
-                                                                                ) ?> </td>
-                                                                        <?php } ?>
-                                                                    </tr>
-                                                                <?php } ?>
-                                                            </tbody>
-                                                        </table>
-                                                    </td>
-                                                </tr>
-                                            <?php } ?>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
                         </div>
                     <?php } ?>
                 </div>
