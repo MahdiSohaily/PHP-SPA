700b55416090d8ba88eec8dcbfae7a8cc382e472
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-25 19:34:58 +0330

    Add a function to calculate the time difference

--- views/Layouts/header.php
+++ views/Layouts/header.php
@@ -21,7 +21,7 @@ date_default_timezone_set("Asia/Tehran");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="../../public/css/styles.css">
+    <link rel="stylesheet" href="./public/css/styles.css">
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
