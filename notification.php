fcbf4074006eeff775cd09b6a8cc767addaebeef
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-26 18:00:15 +0330

    Add notification functionality

--- notification.php
+++ notification.php
@@ -5,53 +5,180 @@ require_once('./views/Layouts/header.php');
 $sql = "SELECT * FROM rates ORDER BY amount ASC";
 $rates = $conn->query($sql);
 ?>
-<div class="py-12">
-    <div class="max-w-full mx-auto sm:px-6 lg:px-8">
-        <div>
-            <div class="p-6 lg:p-8 flex justify-center">
-                <input type="text" name="serial" id="serial" class="rounded-md p-3 text-center w-96 border-2 bg-gray-100" min="0" max="30" onkeyup="search(this.value)" placeholder="... کد فنی قطعه را وارد کنید" />
-            </div>
-            <div class="flex justify-center items-center pb-6">
-                <label for="mode" class="px-2">جستجوی پیشرفته</label>
-                <input type="checkbox" name="super" id="mode" class="rounded-md " />
-            </div>
-            <div class="bg-gray-100 bg-opacity-25">
-                <div class="max-w-7xl overflow-x-auto mx-auto">
-                    <table class="min-w-full text-left text-sm font-light">
-                        <thead class="font-medium dark:border-neutral-500">
-                            <tr class="bg-green-700">
-                                <th scope="col" class="px-3 py-3 bg-black text-white w-52 text-center">
-                                    شماره فنی
-                                </th>
-                                <th scope="col" class="px-3 py-3 text-white w-20">
-                                    دلار پایه
-                                </th>
-                                <th scope="col" class="px-3 py-3 text-white border-black border-r-2">
-                                    +10%
-                                </th>
-                                <?php
-                                if ($rates->num_rows > 0) {
-                                    // output data of each row
-                                    while ($rate = $rates->fetch_assoc()) {
-                                        echo "<th class='" . $rate['status'] . " px-3 py-3 text-white text-center ' scope='col'>" . $rate['amount'] . "</th>";
-                                    }
-                                }
-                                ?>
-                                <th scope="col" class="px-3 py-3 text-white w-32 text-center">
-                                    عملیات
-                                </th>
-                                <th scope="col" class="px-3 py-3 text-white">
-                                    وزن
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody id="results">
+<div class="grid grid-cols-1 md:grid-cols-3 gap-6 px-6 bg-gray-100 bg-opacity-25 mt-20 rtl ">
+    <div v-if="admin" class="max-w-7xl mb-5">
+        <h1 class="text-2xl py-3 rtl" v-if="adminNotification">قیمت های پرسیده شده</h1>
+        <table class="rtl min-w-full text-left text-sm font-light">
+            <thead class="font-medium dark:border-neutral-500">
+                <tr class="bg-green-700">
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        شماره فنی
+                    </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        مشتری
+                    </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        کاربر ارسال کننده
+                    </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        وضعیت
+                    </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center w-24">
+                        عملیات
+                    </th>
+                </tr>
+            </thead>
+            <tbody id="results">
+                <tr v-if="adminNotification.length > 0" v-for="item in adminNotification" class="transition duration-300 ease-in-out bg-neutral-200 hover:bg-neutral-100">
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.code }}
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.customer_name }}
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.user_name }}
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.status === 'pending' ? 'در حال انتظار' : 'بررسی شده' }}
 
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+                    </td>
+
+                    <td class="whitespace-nowrap w-24">
+                        <div class="flex justify-center gap-1 items-center px-2">
+                            <a>
+                                <i @click="travelTO(item.id, item.code, item.customer_id)" class="material-icons text-blue-500 hover:text-blue-700 hover:cursor-pointer">archive</i>
+                            </a>
+                            <a>
+                                <i @click="weDontHave(item.id, item.code, item.customer_id)" class="bold material-icons text-red-500 hover:text-red-700 hover:cursor-pointer">close</i>
+                            </a>
+                        </div>
+                    </td>
+                </tr>
+                <tr v-else class="transition duration-300 ease-in-out bg-neutral-200">
+                    <td colspan="14" class="whitespace-nowrap px-3 py-3 text-center text-red-500 font-bold">
+                        <i class="material-icons text-red-500">mood_bad</i>
+                        <br />
+                        !متاسفانه چیزی برای نمایش در پایگاه داده
+                        موجود نیست
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="max-w-7xl ">
+        <h1 class="text-2xl py-3 rtl">جواب های دریافتی</h1>
+        <table class="rtl min-w-full text-left text-sm font-light">
+            <thead class="font-medium dark:border-neutral-500">
+                <tr class="bg-green-700">
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        شماره فنی
+                    </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        مشتری
+                    </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        کاربر ارسال کننده
+                    </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        قیمت داده شده </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center w-24">
+                        عملیات
+                    </th>
+                </tr>
+            </thead>
+            <tbody id="results">
+                <tr v-if="answeredNotifications.length > 0" v-for="item in answeredNotifications" class="transition duration-300 ease-in-out bg-neutral-200 hover:bg-neutral-100">
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.code }}
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.customer_name }}
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.user_name }}
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.price }}
+
+                    </td>
+
+                    <td class="whitespace-nowrap w-24">
+                        <div class="flex justify-center gap-1 items-center px-2">
+                            <a>
+                                <i @click="markUsRead(item.id)" class="material-icons text-blue-500 hover:text-blue-700 hover:cursor-pointer">remove_red_eye</i>
+                            </a>
+
+                        </div>
+                    </td>
+                </tr>
+                <tr v-else class="transition duration-300 ease-in-out bg-neutral-200">
+                    <td colspan="14" class="whitespace-nowrap px-3 py-3 text-center text-red-500 font-bold">
+                        <i class="material-icons text-red-500">mood_bad</i>
+                        <br />
+                        !متاسفانه چیزی برای نمایش در پایگاه داده
+                        موجود نیست
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="max-w-7xl ">
+        <h1 class="text-2xl py-3 rtl">مشاهده شده</h1>
+        <table class="rtl min-w-full text-left text-sm font-light">
+            <thead class="font-medium dark:border-neutral-500">
+                <tr class="bg-green-700">
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        شماره فنی
+                    </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        مشتری
+                    </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        کاربر ارسال کننده
+                    </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center">
+                        قیمت داده شده </th>
+                    <th scope="col" class="px-3 py-3 text-white text-center w-24">
+                        عملیات
+                    </th>
+                </tr>
+            </thead>
+            <tbody id="results">
+                <tr v-if="previousNotifications.length > 0" v-for="item in previousNotifications" class="transition duration-300 ease-in-out bg-neutral-200 hover:bg-neutral-100">
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.code }}
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.customer_name }}
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.user_name }}
+                    </td>
+                    <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
+                        {{ item.price }}
+
+                    </td>
+
+                    <td class="whitespace-nowrap w-24">
+                        <div class="flex justify-center gap-1 items-center px-2">
+                            <a>
+                                <i @click="markUsRead(item.id)" class="material-icons text-blue-500 hover:text-blue-700 hover:cursor-pointer">remove_red_eye</i>
+                            </a>
+
+                        </div>
+                    </td>
+                </tr>
+                <tr v-else class="transition duration-300 ease-in-out bg-neutral-200">
+                    <td colspan="14" class="whitespace-nowrap px-3 py-3 text-center text-red-500 font-bold">
+                        <i class="material-icons text-red-500">mood_bad</i>
+                        <br />
+                        !متاسفانه چیزی برای نمایش در پایگاه داده
+                        موجود نیست
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 <script>
