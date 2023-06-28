c626a96463d6ea02ee5be9ea05e7b979d15b36d1
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-28 13:09:11 +0330

    Add ajax new method to update the govin price section aysnc

--- views/Layouts/header.php
+++ views/Layouts/header.php
@@ -138,15 +138,15 @@ $_SESSION["user_id"] = $_SESSION["id"];
                         <i class="px-2 material-icons hover:cursor-pointer">receipt</i>
                         قیمت دهی
                     </a>
-                    <a class="cursor-pointer inline-flex items-center py-3 pr-6 text-sm font-medium leading-5 text-gray-500 hover:bg-indigo-500 hover:text-white focus:outline-none transition duration-150 ease-in-out" href="../../../main.php">
-                        <i class="px-2 material-icons hover:cursor-pointer">portrait</i>
-                        حساب کاربری
+                    <a class="cursor-pointer inline-flex items-center py-3 pr-6 text-sm font-medium leading-5 text-gray-500 hover:bg-indigo-500 hover:text-white focus:outline-none transition duration-150 ease-in-out" href="../main.php">
+                        <i class="px-2 material-icons hover:cursor-pointer">call</i>
+                        مرکز تماس 
                     </a>
                 </ul>
                 <!-- Authentication -->
-                <a class="cursor-pointer inline-flex items-center py-3 pr-6 text-sm font-medium leading-5 text-gray-500 hover:bg-indigo-500 hover:text-white focus:outline-none transition duration-150 ease-in-out" href="../../1402/logout.php">
+                <a class="rtl cursor-pointer inline-flex items-center py-3 pr-6 text-sm font-medium leading-5 text-gray-500 hover:bg-indigo-500 hover:text-white focus:outline-none transition duration-150 ease-in-out" href="../../1402/logout.php">
                     <i class="px-2 material-icons hover:cursor-pointer">power_settings_new</i>
-                    حساب کاربری
+                    خروج از حساب
                 </a>
             </nav>
             <!-- Page Content -->
