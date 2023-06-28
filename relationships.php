55ee23c0ce7c252f1f70879eb330184494f6b763
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-28 15:37:15 +0330

    Add select 2 for selecting options

--- relationships.php
+++ relationships.php
@@ -114,41 +114,6 @@ $status = $conn->query($status_sql);
                         } ?>
                     </select>
                 </div>
-                <script>
-                    // In your Javascript (external .js resource or <script> tag)
-                    $(document).ready(function() {
-                        $('#cars').select2({
-                            matcher: matchCustom
-                        });
-
-                    });
-
-                    function matchCustom(params, data) {
-                        // If there are no search terms, return all of the data
-                        if ($.trim(params.term) === '') {
-                            return data;
-                        }
-
-                        // Do not display the item if there is no 'text' property
-                        if (typeof data.text === 'undefined') {
-                            return null;
-                        }
-
-                        // `params.term` should be the term that is used for searching
-                        // `data.text` is the text that is displayed for the data object
-                        if (data.text.indexOf(params.term) > -1) {
-                            var modifiedData = $.extend({}, data, true);
-                            modifiedData.text += ' (matched)';
-
-                            // You can return modified objects from here
-                            // This includes matching the `children` how you want in nested data sets
-                            return modifiedData;
-                        }
-
-                        // Return `null` if the term should not be displayed
-                        return null;
-                    }
-                </script>
                 <div class="col-span-12 sm:col-span-4 mb-3">
                     <label for="cars">
                         وضعیت
@@ -232,7 +197,7 @@ $status = $conn->query($status_sql);
                         console.log(error);
                     });
             } else {
-                resultBox.innerHTML = "کد فنی وارد شده فقد اعتبار است";
+                resultBox.innerHTML = "کد فنی وارد شده فاقد اعتبار است";
             }
         } else {
             resultBox.innerHTML = "";
@@ -445,72 +410,105 @@ $status = $conn->query($status_sql);
             }
         }
     }
-
-    jQuery(function() {
-        jQuery('.multiSelect').each(function(e) {
-            var self = jQuery(this);
-            var field = self.find('.multiSelect_field');
-            var fieldOption = field.find('option');
-            var placeholder = field.attr('data-placeholder');
-
-            field.hide().after(`<div class="multiSelect_dropdown"></div>
-                        <span class="multiSelect_placeholder">` + placeholder + `</span>
-                        <ul class="multiSelect_list"></ul>
-                        <span class="multiSelect_arrow"></span>`);
-
-            fieldOption.each(function(e) {
-                jQuery('.multiSelect_list').append(`<li class="multiSelect_option" data-value="` + jQuery(this).val() + `">
-                                            <a class="multiSelect_text">` + jQuery(this).text() + `</a>
-                                          </li>`);
-            });
-
-            var dropdown = self.find('.multiSelect_dropdown');
-            var list = self.find('.multiSelect_list');
-            var option = self.find('.multiSelect_option');
-            var optionText = self.find('.multiSelect_text');
-
-            dropdown.attr('data-multiple', 'true');
-            list.css('top', dropdown.height() + 5);
-
-            option.click(function(e) {
-                var self = jQuery(this);
-                e.stopPropagation();
-                self.addClass('-selected');
-                field.find('option:contains(' + self.children().text() + ')').prop('selected', true);
-                dropdown.append(function(e) {
-                    return jQuery('<span class="multiSelect_choice">' + self.children().text() + '<svg class="multiSelect_deselect -iconX"><use href="#iconX"></use></svg></span>').click(function(e) {
-                        var self = jQuery(this);
-                        e.stopPropagation();
-                        self.remove();
-                        list.find('.multiSelect_option:contains(' + self.text() + ')').removeClass('-selected');
-                        list.css('top', dropdown.height() + 5).find('.multiSelect_noselections').remove();
-                        field.find('option:contains(' + self.text() + ')').prop('selected', false);
-                        if (dropdown.children(':visible').length === 0) {
-                            dropdown.removeClass('-hasValue');
-                        }
-                    });
-                }).addClass('-hasValue');
-                list.css('top', dropdown.height() + 5);
-                if (!option.not('.-selected').length) {
-                    list.append('<h5 class="multiSelect_noselections">No Selections</h5>');
-                }
-            });
-
-            dropdown.click(function(e) {
-                e.stopPropagation();
-                e.preventDefault();
-                dropdown.toggleClass('-open');
-                list.toggleClass('-open').scrollTop(0).css('top', dropdown.height() + 5);
-            });
-
-            jQuery(document).on('click touch', function(e) {
-                if (dropdown.hasClass('-open')) {
-                    dropdown.toggleClass('-open');
-                    list.removeClass('-open');
-                }
-            });
+    // In your Javascript (external .js resource or <script> tag)
+    $(document).ready(function() {
+        $('#cars').select2({
+            matcher: matchCustom
         });
+
     });
+
+    function matchCustom(params, data) {
+        // If there are no search terms, return all of the data
+        if ($.trim(params.term) === '') {
+            return data;
+        }
+
+        // Do not display the item if there is no 'text' property
+        if (typeof data.text === 'undefined') {
+            return null;
+        }
+
+        // `params.term` should be the term that is used for searching
+        // `data.text` is the text that is displayed for the data object
+        if (data.text.indexOf(params.term) > -1) {
+            var modifiedData = $.extend({}, data, true);
+            modifiedData.text += ' (matched)';
+
+            // You can return modified objects from here
+            // This includes matching the `children` how you want in nested data sets
+            return modifiedData;
+        }
+
+        // Return `null` if the term should not be displayed
+        return null;
+    }
+
+    // jQuery(function() {
+    //     jQuery('.multiSelect').each(function(e) {
+    //         var self = jQuery(this);
+    //         var field = self.find('.multiSelect_field');
+    //         var fieldOption = field.find('option');
+    //         var placeholder = field.attr('data-placeholder');
+
+    //         field.hide().after(`<div class="multiSelect_dropdown"></div>
+    //                     <span class="multiSelect_placeholder">` + placeholder + `</span>
+    //                     <ul class="multiSelect_list"></ul>
+    //                     <span class="multiSelect_arrow"></span>`);
+
+    //         fieldOption.each(function(e) {
+    //             jQuery('.multiSelect_list').append(`<li class="multiSelect_option" data-value="` + jQuery(this).val() + `">
+    //                                         <a class="multiSelect_text">` + jQuery(this).text() + `</a>
+    //                                       </li>`);
+    //         });
+
+    //         var dropdown = self.find('.multiSelect_dropdown');
+    //         var list = self.find('.multiSelect_list');
+    //         var option = self.find('.multiSelect_option');
+    //         var optionText = self.find('.multiSelect_text');
+
+    //         dropdown.attr('data-multiple', 'true');
+    //         list.css('top', dropdown.height() + 5);
+
+    //         option.click(function(e) {
+    //             var self = jQuery(this);
+    //             e.stopPropagation();
+    //             self.addClass('-selected');
+    //             field.find('option:contains(' + self.children().text() + ')').prop('selected', true);
+    //             dropdown.append(function(e) {
+    //                 return jQuery('<span class="multiSelect_choice">' + self.children().text() + '<svg class="multiSelect_deselect -iconX"><use href="#iconX"></use></svg></span>').click(function(e) {
+    //                     var self = jQuery(this);
+    //                     e.stopPropagation();
+    //                     self.remove();
+    //                     list.find('.multiSelect_option:contains(' + self.text() + ')').removeClass('-selected');
+    //                     list.css('top', dropdown.height() + 5).find('.multiSelect_noselections').remove();
+    //                     field.find('option:contains(' + self.text() + ')').prop('selected', false);
+    //                     if (dropdown.children(':visible').length === 0) {
+    //                         dropdown.removeClass('-hasValue');
+    //                     }
+    //                 });
+    //             }).addClass('-hasValue');
+    //             list.css('top', dropdown.height() + 5);
+    //             if (!option.not('.-selected').length) {
+    //                 list.append('<h5 class="multiSelect_noselections">No Selections</h5>');
+    //             }
+    //         });
+
+    //         dropdown.click(function(e) {
+    //             e.stopPropagation();
+    //             e.preventDefault();
+    //             dropdown.toggleClass('-open');
+    //             list.toggleClass('-open').scrollTop(0).css('top', dropdown.height() + 5);
+    //         });
+
+    //         jQuery(document).on('click touch', function(e) {
+    //             if (dropdown.hasClass('-open')) {
+    //                 dropdown.toggleClass('-open');
+    //                 list.removeClass('-open');
+    //             }
+    //         });
+    //     });
+    // });
 </script>
 <?php
 require_once('./views/Layouts/footer.php');
