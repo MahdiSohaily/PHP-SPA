4c8a4ed51ed46fb2112ebc4b2239c04830d437b6
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-07-01 11:27:26 +0330

    Add functionality to copy the ordered price

--- showPriceReports.php
+++ showPriceReports.php
@@ -12,6 +12,8 @@ if ($isValidCustomer) {
         $completeCode = $finalResult['completeCode'];
         $notification = $finalResult['notification'];
         $rates = $finalResult['rates'];
+
+        print_r(json_encode($finalResult));
 ?>
         <div class="rtl col-md-6 mx-auto">
             <table class="min-w-full text-left text-sm font-light custom-table">
