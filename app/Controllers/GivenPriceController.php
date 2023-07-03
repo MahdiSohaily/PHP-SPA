db233538da4c337958e52ac1d67538e825ae3f28
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-07-03 11:26:07 +0330

    Update the given price sql

--- app/Controllers/GivenPriceController.php
+++ app/Controllers/GivenPriceController.php
@@ -256,9 +256,8 @@ function givenPrice($conn, $code, $relation_exist = null)
         $ordared_price['ordered'] = true;
     }
 
-    $sql = "SELECT 
-    prices.price, prices.partnumber, users.username, prices.created_at
-    FROM ((shop.prices 
+    $sql = "SELECT prices.price, prices.partnumber, customer.name, customer.family ,users.username, prices.created_at
+    FROM ((prices 
     INNER JOIN callcenter.customer ON customer.id = prices.customer_id )
     INNER JOIN yadakshop1402.users ON users.id = prices.user_id)
     WHERE partnumber LIKE '" . $code . "' ORDER BY created_at DESC LIMIT 7";
