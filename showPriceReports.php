0a662362a298e14dedbff6cde158f79487c62ed0
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-07-04 11:46:35 +0330

    Add given price history page

--- showPriceReports.php
+++ showPriceReports.php
@@ -311,7 +311,7 @@ if ($isValidCustomer) {
                                                                     ?>
                                                                 </td>
                                                                 </tr>
-                                                                <tr class="min-w-full mb-1 border-b-2 <?php echo array_key_exists("ordered", $price) ? 'bg-red-500' : 'bg-indigo-300' ?>" data-price='<?php echo $price['price'] ?>'>
+                                                                <tr class="min-w-full mb-1 border-b-2 <?php echo array_key_exists("ordered", $price) || $price['customerID'] == 1 ? 'bg-red-500' : 'bg-indigo-300' ?>" data-price='<?php echo $price['price'] ?>'>
                                                                     <td class="<?php array_key_exists("ordered", $price) ? 'text-white' : '' ?> text-gray-800 px-2 tiny-text" colspan="3" scope="col">
                                                                         <div class="rtl flex items-center w-full <?php echo array_key_exists("ordered", $price) ? 'text-white' : 'text-gray-800' ?>">
                                                                             <i class="px-1 material-icons tiny-text <?php echo array_key_exists("ordered", $price) ? 'text-white' : 'text-gray-800' ?>">access_time</i>
