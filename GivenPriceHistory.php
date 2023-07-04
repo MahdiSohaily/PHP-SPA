454fa1c7ed229ac14803a82ae577df20aa44dfca
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-07-04 10:38:05 +0330

    Add given price history page

--- GivenPriceHistory.php
+++ GivenPriceHistory.php
@@ -3,57 +3,101 @@ require_once './database/connect.php';
 require_once('./views/Layouts/header.php');
 require_once('./app/Controllers/GivenPriceHistoryController.php');
 ?>
-<div class="rtl col-6 mx-auto">
-    <table class="min-w-full text-left text-sm font-light custom-table mb-2">
-        <thead class="font-medium bg-green-600">
-            <tr>
-                <th scope="col" class="px-3 py-3 text-white text-center">
-                    نام
-                </th>
-                <th scope="col" class="px-3 py-3 text-white text-center">
-                    نام خانوادگی
-                </th>
-                <th scope="col" class="px-3 py-3 text-white text-center">
-                    شماره تماس
-                </th>
-                <th scope="col" class="px-3 py-3 text-white text-center">
-                    ماشین
-                </th>
-                <th scope="col" class="px-3 py-3 text-white text-center">
-                    آدرس
-                </th>
-            </tr>
-        </thead>
-        <tbody class="bg-white">
-            <tr class="relative">
-                <td class=" px-1">
-                    <p class="text-center bold text-gray-700 px-2 py-3">
-                        <?php echo $customer_info['name'] ?>
-                    </p>
-                </td>
-                <td class=" px-1">
-                    <p class="text-center bold text-gray-700 px-2 py-3">
-                        <?php echo $customer_info['family'] ?>
-                    </p>
-                </td>
-                <td class=" px-1">
-                    <p class="text-center bold text-gray-700 px-2 py-3">
-                        <?php echo $customer_info['phone'] ?>
-                    </p>
-                </td>
-                <td class=" px-1">
-                    <p class="text-center bold text-gray-700 px-2 py-3">
-                        <?php echo $customer_info['car'] ?>
-                    </p>
-                </td>
-                <td class=" px-1">
-                    <p class="text-center bold text-gray-700 px-2 py-3">
-                        <?php echo $customer_info['address'] ?>
-                    </p>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+<!-- START NEWLY ADDED SECTION BY MAHDI REZAEI -->
+<div id="child_three">
+    <h2 class="title">قیمت های داده شده</h2>
+
+    <div class="rtl col-8 mx-auto">
+        <table class="min-w-full text-left text-sm bg-white custom-table mb-2 p-3">
+            <thead class="font-medium bg-green-600">
+                <tr>
+                    <th scope="col" class="px-2 py-2 text-white text-right">
+                        قیمت
+                    </th>
+                    <th scope="col" class="px-2 py-2 text-white text-right">
+                        کد فنی
+                    </th>
+                    <th scope="col" class="px-2 py-2 text-white text-right">
+                        قیمت دهنده
+                    </th>
+                    <th scope="col" class="px-2 py-2 text-white text-right">
+                        زمان
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if (count($givenPrice) > 0) {
+                ?>
+                    <?php foreach ($givenPrice as $price) { ?>
+                        <?php if ($price['price'] !== null) {
+                        ?>
+                            <tr class="tiny-text min-w-full mb-1 ?>">
+                            <?php  } ?>
+                            <td class="">
+                                <p class="text-right bold text-gray-700 px-2 py-1">
+                                    <?php echo $price['price'] === null ? 'ندارد' : $price['price']  ?>
+                                </p>
+                            </td>
+                            <td class="">
+                                <p class="text-right bold text-gray-700 px-2 py-1">
+                                    <?php echo $price['partnumber']; ?>
+                                </p>
+                            </td>
+                            <td style="width:100px;">
+                                <p class="text-right bold text-gray-700 px-2 py-1">
+                                    <img title="<?php echo $price['username'] ?>" class="userImage" src="../../userimg/<?php echo $price['userID'] ?>.jpg" alt="userimage">
+                                </p>
+                            </td>
+                            <td class="time">
+                                <p class="text-right bold text-gray-700 px-2 py-1">
+                                    <?php
+                                    date_default_timezone_set("Asia/Tehran");
+                                    $create = date($price['created_at']);
+
+                                    $now = new DateTime(); // current date time
+                                    $date_time = new DateTime($create); // date time from string
+                                    $interval = $now->diff($date_time); // difference between two date times
+                                    $days = $interval->format('%a'); // difference in days
+                                    $hours = $interval->format('%h'); // difference in hours
+                                    $minutes = $interval->format('%i'); // difference in minutes
+                                    $seconds = $interval->format('%s'); // difference in seconds
+
+                                    $text = '';
+
+                                    if ($days) {
+                                        $text .= " $days روز و ";
+                                    }
+
+                                    if ($hours) {
+                                        $text .= "$hours ساعت ";
+                                    }
+
+                                    if ($minutes) {
+                                        $text .= "$minutes دقیقه ";
+                                    }
+
+                                    if ($seconds) {
+                                        $text .= "$seconds ثانیه ";
+                                    }
+
+                                    echo "$text قبل";
+                                    ?>
+                                </p>
+                            </td>
+                            </tr>
+                        <?php
+                    } ?>
+                    <?php } else { ?>
+                        <tr class="">
+                            <td colspan="4" scope="col" class="not-exist">
+                                موردی برای نمایش وجود ندارد !!
+                            </td>
+                        </tr>
+                    <?php } ?>
+            </tbody>
+        </table>
+    </div>
 </div>
 
 <?php
---
+++ app/Controllers/GivenPriceHistoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+$givenPrice = givenPrice($conn);
+function givenPrice($conn)
+{
+    $sql = "SELECT 
+    prices.price, prices.partnumber, users.username, users.id as userID, prices.created_at
+    FROM ((shop.prices 
+    INNER JOIN callcenter.customer ON customer.id = prices.customer_id )
+    INNER JOIN yadakshop1402.users ON users.id = prices.user_id)
+    ORDER BY prices.created_at DESC LIMIT 200";
+    $result = mysqli_query($conn, $sql);
+
+
+    $givenPrices = [];
+    if (mysqli_num_rows($result) > 0) {
+        while ($item = mysqli_fetch_assoc($result)) {
+            array_push($givenPrices, $item);
+        }
+    }
+    return  $givenPrices;
+}
