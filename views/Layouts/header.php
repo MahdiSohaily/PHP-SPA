27dd6685797a616f8dda6982b60606421e44baa1
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-07-08 11:54:27 +0330

    Update the main nav address

--- views/Layouts/header.php
+++ views/Layouts/header.php
@@ -182,7 +182,7 @@ $_SESSION["user_id"] = $_SESSION["id"];
                         <i class="px-2 material-icons hover:cursor-pointer">receipt</i>
                         قیمت دهی دستوری
                     </a>
-                    <a class="cursor-pointer inline-flex items-center py-3 pr-6 text-sm font-medium leading-5 text-gray-500 hover:bg-indigo-500 hover:text-white focus:outline-none transition duration-150 ease-in-out" href="../main.php">
+                    <a class="cursor-pointer inline-flex items-center py-3 pr-6 text-sm font-medium leading-5 text-gray-500 hover:bg-indigo-500 hover:text-white focus:outline-none transition duration-150 ease-in-out" href="../index.php">
                         <i class="px-2 material-icons hover:cursor-pointer">call</i>
                         مرکز تماس
                     </a>
