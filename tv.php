0ddf2a6ad30b450adaa6352e4ae1fdb6092d69ee
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-07-09 09:39:46 +0330

    Add new column to the

--- tv.php
+++ tv.php
@@ -13,7 +13,7 @@
          <a href="?user=1 or 1=1">همه</a>
      </div>
  </div>
- <div class="grid grid-cols-1 md:grid-cols-4 gap-6 lg:gap-6 lg:p-2 overflow-auto">
+ <div class="grid grid-cols-1 md:grid-cols-4 gap-2 lg:gap-2 lg:p-2 overflow-auto">
      <div class="">
          <h2 class="title">تماس های ورودی</h2>
          <table class=text-sm bg-white custom-table mb-2 p-3">
@@ -180,128 +180,218 @@
          </table>
      </div>
      <div class="">
-
-     </div>
-     <div>
-         <div class="">
-             <h2 class="text-xl py-2 bold">آخرین قیمت های داده شده</h2>
-             <table class="text-sm bg-white custom-table mb-2 p-3">
-                 <thead class="font-medium bg-green-600">
-                     <tr>
-                         <th style="width: 300px;" scope="col" class="px-3 py-2 text-white text-right">
-                             مشتری
-                         </th>
-                         <th style="width: 80px !important" scope="col" class="px-3 py-2 text-white text-right">
-                             قیمت
-                         </th>
-                         <th scope="col" class="px-3 py-2 text-white text-right">
-                             کد فنی
-                         </th>
-                         <th scope="col" class="px-3 py-2 text-white text-center">
-                             قیمت دهنده
-                         </th>
-                         <th style="width: 300px;" scope="col" class="px-3 py-2 text-white text-right">
-                             زمان
-                         </th>
-                     </tr>
-                 </thead>
-                 <tbody>
-                     <?php
-                        $givenPrice = givenPrice($con);
-                        function givenPrice($con)
-                        {
-                            $sql = "SELECT 
+         <h2 class="title">آخرین قیمت های داده شده</h2>
+         <table class="text-sm bg-white custom-table mb-2 p-3">
+             <thead class="font-medium bg-green-600">
+                 <tr>
+                     <th style="width: 300px;" scope="col" class="px-3 py-2 text-white text-right">
+                         مشتری
+                     </th>
+                     <th style="width: 80px !important" scope="col" class="px-3 py-2 text-white text-right">
+                         قیمت
+                     </th>
+                     <th scope="col" class="px-3 py-2 text-white text-right">
+                         کد فنی
+                     </th>
+                     <th scope="col" class="px-3 py-2 text-white text-center">
+                        کاربر
+                     </th>
+                     <th style="width: 300px;" scope="col" class="px-3 py-2 text-white text-right">
+                         زمان
+                     </th>
+                 </tr>
+             </thead>
+             <tbody>
+                 <?php
+                    $givenPrice = givenPrice($con);
+                    function givenPrice($con)
+                    {
+                        $sql = "SELECT 
                          prices.price, prices.partnumber, users.username,customer.id AS customerID, users.id as userID, prices.created_at, customer.name, customer.family
                          FROM ((shop.prices 
                          INNER JOIN callcenter.customer ON customer.id = prices.customer_id )
                          INNER JOIN yadakshop1402.users ON users.id = prices.user_id)
                          ORDER BY prices.created_at DESC LIMIT 200";
-                            $result = mysqli_query($con, $sql);
+                        $result = mysqli_query($con, $sql);
 
 
-                            $givenPrices = [];
-                            if (mysqli_num_rows($result) > 0) {
-                                while ($item = mysqli_fetch_assoc($result)) {
-                                    array_push($givenPrices, $item);
-                                }
+                        $givenPrices = [];
+                        if (mysqli_num_rows($result) > 0) {
+                            while ($item = mysqli_fetch_assoc($result)) {
+                                array_push($givenPrices, $item);
                             }
-                            return  $givenPrices;
                         }
+                        return  $givenPrices;
+                    }
 
-                        if (count($givenPrice) > 0) {
-                        ?>
-                         <?php foreach ($givenPrice as $price) { ?>
-                             <?php if ($price['price'] !== null) {
-                                ?>
-                                 <tr class="mb-1 ?> odd:bg-gray-200">
-                                 <?php  } ?>
-                                 <td class="tiny-text bold px-1">
-                                     <p class="text-right bold text-gray-700 px-2 py-1">
-                                         <?php echo $price['name'] . ' ' . $price['family'] ?>
-                                     </p>
-                                 </td>
-                                 <td style="width: 100px;" class="tiny-text bold px-1">
-                                     <p class="text-right bold text-gray-700 px-2 py-1">
-                                         <?php echo $price['price'] === null ? 'ندارد' : $price['price']  ?>
-                                     </p>
-                                 </td>
-                                 <td class="tiny-text bold px-1">
-                                     <p class="text-right bold text-gray-700 px-2 py-1">
-                                         <?php echo $price['partnumber']; ?>
-                                     </p>
-                                 </td>
-                                 <td style="width:120px;">
-                                     <p class="text-center bold text-gray-700 px-2 py-1">
-                                         <img title="<?php echo $price['username'] ?>" class="userImage mx-auto" src="../userimg/<?php echo $price['userID'] ?>.jpg" alt="userimage">
-                                     </p>
-                                 </td>
-                                 <td class="tiny-text bold time">
-                                     <p class="text-right bold text-gray-700 px-2 py-1">
-                                         <?php
-                                            date_default_timezone_set("Asia/Tehran");
-                                            $create = date($price['created_at']);
+                    if (count($givenPrice) > 0) {
+                    ?>
+                     <?php foreach ($givenPrice as $price) { ?>
+                         <?php if ($price['price'] !== null) {
+                            ?>
+                             <tr class="mb-1 ?> odd:bg-gray-200">
+                             <?php  } ?>
+                             <td class="tiny-text bold px-1">
+                                 <p class="text-right bold text-gray-700 px-2 py-1">
+                                     <?php echo $price['name'] . ' ' . $price['family'] ?>
+                                 </p>
+                             </td>
+                             <td style="width: 100px;" class="tiny-text bold px-1">
+                                 <p class="text-right bold text-gray-700 px-2 py-1">
+                                     <?php echo $price['price'] === null ? 'ندارد' : $price['price']  ?>
+                                 </p>
+                             </td>
+                             <td class="tiny-text bold px-1">
+                                 <p class="text-right bold text-gray-700 px-2 py-1">
+                                     <?php echo $price['partnumber']; ?>
+                                 </p>
+                             </td>
+                             <td style="width:120px;">
+                                 <p class="text-center bold text-gray-700 px-2 py-1">
+                                     <img title="<?php echo $price['username'] ?>" class="userImage mx-auto" src="../userimg/<?php echo $price['userID'] ?>.jpg" alt="userimage">
+                                 </p>
+                             </td>
+                             <td class="tiny-text bold time">
+                                 <p class="text-right bold text-gray-700 px-2 py-1">
+                                     <?php
+                                        date_default_timezone_set("Asia/Tehran");
+                                        $create = date($price['created_at']);
 
-                                            $now = new DateTime(); // current date time
-                                            $date_time = new DateTime($create); // date time from string
-                                            $interval = $now->diff($date_time); // difference between two date times
-                                            $days = $interval->format('%a'); // difference in days
-                                            $hours = $interval->format('%h'); // difference in hours
-                                            $minutes = $interval->format('%i'); // difference in minutes
-                                            $seconds = $interval->format('%s'); // difference in seconds
+                                        $now = new DateTime(); // current date time
+                                        $date_time = new DateTime($create); // date time from string
+                                        $interval = $now->diff($date_time); // difference between two date times
+                                        $days = $interval->format('%a'); // difference in days
+                                        $hours = $interval->format('%h'); // difference in hours
+                                        $minutes = $interval->format('%i'); // difference in minutes
+                                        $seconds = $interval->format('%s'); // difference in seconds
 
-                                            $text = '';
+                                        $text = '';
 
-                                            if ($days) {
-                                                $text .= " $days روز و ";
-                                            }
+                                        if ($days) {
+                                            $text .= " $days روز و ";
+                                        }
 
-                                            if ($hours) {
-                                                $text .= "$hours ساعت ";
-                                            }
+                                        if ($hours) {
+                                            $text .= "$hours ساعت ";
+                                        }
 
-                                            if (!$days && $minutes) {
-                                                $text .= "$minutes دقیقه ";
-                                            }
+                                        if (!$days && $minutes) {
+                                            $text .= "$minutes دقیقه ";
+                                        }
 
-                                            if (!$days && !$hours && $seconds) {
-                                                $text .= "$seconds ثانیه ";
-                                            }
+                                        if (!$days && !$hours && $seconds) {
+                                            $text .= "$seconds ثانیه ";
+                                        }
 
-                                            echo "$text قبل";
-                                            ?>
-                                     </p>
-                                 </td>
-                                 </tr>
-                             <?php
-                            } ?>
-                         <?php } else { ?>
-                             <tr class="">
-                                 <td colspan="4" scope="col" class="not-exist">
-                                     موردی برای نمایش وجود ندارد !!
-                                 </td>
+                                        echo "$text قبل";
+                                        ?>
+                                 </p>
+                             </td>
                              </tr>
-                         <?php } ?>
-                 </tbody>
+                         <?php
+                        } ?>
+                     <?php } else { ?>
+                         <tr class="">
+                             <td colspan="4" scope="col" class="not-exist">
+                                 موردی برای نمایش وجود ندارد !!
+                             </td>
+                         </tr>
+                     <?php } ?>
+             </tbody>
+         </table>
+     </div>
+     <div class="">
+         <h2 class="title">آخرین استعلام ها</h2>
+         <div class="">
+
+             <table class="">
+                 <thead>
+                     <tr class="tiny-text bg-violet-800 text-white">
+                         <th class="p-2">مشتری</th>
+                         <th class="p-2">تلفن</th>
+                         <th class="p-2">اطلاعات استعلام</th>
+                         <th class="p-2">کاربر</th>
+                         <th class="p-2">زمان</th>
+                         <!-- <th class="p-2">تاریخ</th> -->
+                     </tr>
+                 </thead>
+
+                 <?php
+                    $sql2 = "SELECT * FROM record ORDER BY  time DESC LIMIT 350  ";
+                    $result2 = mysqli_query($con, $sql2);
+                    if (mysqli_num_rows($result2) > 0) {
+                        while ($row2 = mysqli_fetch_assoc($result2)) {
+                            $time = $row2['time'];
+                            $callinfo = $row2['callinfo'];
+                            $user = $row2['user'];
+                            $phone = $row2['phone'];
+
+                            $sql = "SELECT * FROM customer WHERE phone LIKE '" . $phone . "%'";
+                            $result = mysqli_query($con, $sql);
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $name = $row['name'];
+                                    $family = $row['family'];
+                    ?>
+                                 <tbody>
+                                     <tr>
+                                         <td class="tiny-text p-2"><?php echo ($name . " " . $family) ?></td>
+                                         <td class="tiny-text p-2"><a target="_blank" href="main.php?phone=<?php echo $phone ?>"><?php echo $phone ?></a></td>
+                                         <td class="tiny-text p-2"><?php echo nl2br($callinfo) ?></td>
+                                         <td style="width: 50px;" class="tiny-text p-2"><img class="userImage mx-auto" src="../userimg/<?php echo $user ?>.jpg" />
+                                     <?php
+                                    }
+                                }
+
+                                date_default_timezone_set('Asia/Tehran');
+
+                                $datetime1 = new DateTime();
+                                $datetime2 = new DateTime($time);
+                                $interval = $datetime1->diff($datetime2);
+                                        ?>
+                                         </td>
+
+                                         <td style="width: 100px;" class="tiny-text p-2"><?php
+                                         $create = date($time);
+
+                                         $now = new DateTime(); // current date time
+                                         $date_time = new DateTime($create); // date time from string
+                                         $interval = $now->diff($date_time); // difference between two date times
+                                         $days = $interval->format('%a'); // difference in days
+                                         $hours = $interval->format('%h'); // difference in hours
+                                         $minutes = $interval->format('%i'); // difference in minutes
+                                         $seconds = $interval->format('%s'); // difference in seconds
+ 
+                                         $text = '';
+ 
+                                         if ($days) {
+                                             $text .= " $days روز و ";
+                                         }
+ 
+                                         if ($hours) {
+                                             $text .= "$hours ساعت ";
+                                         }
+ 
+                                         if (!$days && $minutes) {
+                                             $text .= "$minutes دقیقه ";
+                                         }
+ 
+                                         if (!$days && !$hours && $seconds) {
+                                             $text .= "$seconds ثانیه ";
+                                         }
+                                         echo "$text قبل";
+                                         ?>
+                                          </td>
+                                         <!-- <td class="record-date"><?php echo $time ?></td> -->
+                                     </tr>
+                                 </tbody>
+                         <?php
+
+                        }
+                    } else {
+                        echo '<td colspan="4">هیچ اطلاعاتی موجود نیست</td>';
+                    }
+                            ?>
              </table>
          </div>
      </div>
