574a2050a2f7df9f1b6eaf000811862b46157092
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-07-09 16:26:35 +0330

    Add shomare factor ajax

--- factorAjax.php
+++ factorAjax.php
@@ -14,27 +14,40 @@ if (isset($_POST['getFactor'])) {
     // $sql = "SELECT * FROM shomarefaktor WHERE time < '$start' AND time >= '$end' ORDER BY shomare DESC";
     $sql = "SELECT * FROM shomarefaktor WHERE time < '2023-05-10 12:00:00' AND time >= '2023-05-09 12:00:00' ORDER BY shomare DESC";
     $result = mysqli_query($con, $sql);
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $shomare = $row['shomare'];
-            $kharidar = $row['kharidar'];
-            $user = $row['user'];
+
+
 ?>
-            <tr>
-                <td>
-                    <div class="jadval-shomare-blue"><?php echo $shomare ?></div>
-                </td>
-                <td>
-                    <div class="jadval-shomare-kharidar"><?php echo $kharidar ?></div>
-                </td>
-                <td><img class="user-img" src="../userimg/<?php echo $user ?>.jpg" /></td>
-            </tr>
-    <?php
 
-        }
-    }
+    <table class="customer-list jadval-shomare">
+        <tr>
+            <th>شماره فاکتور</th>
+            <th>خریدار</th>
+            <th>کاربر</th>
+        </tr>
+        <tbody>
+            <?php
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $shomare = $row['shomare'];
+                    $kharidar = $row['kharidar'];
+                    $user = $row['user'];
+            ?>
+                    <tr>
+                        <td>
+                            <div class="jadval-shomare-blue"><?php echo $shomare ?></div>
+                        </td>
+                        <td>
+                            <div class="jadval-shomare-kharidar"><?php echo $kharidar ?></div>
+                        </td>
+                        <td><img class="user-img" src="../userimg/<?php echo $user ?>.jpg" /></td>
+                    </tr>
+            <?php
 
-    ?>
+                }
+            }
 
+            ?>
+        </tbody>
+    </table>
 <?php
 }
--- shomarefaktor.php
+++ shomarefaktor.php
@@ -92,45 +92,46 @@ if (!empty($_GET['date'])) {
             ?>
         </div>
     </div>
-    <table class="customer-list jadval-shomare">
-        <tr>
-            <th>شماره فاکتور</th>
-            <th>خریدار</th>
-            <th>کاربر</th>
-            <th>ویرایش</th>
-        </tr>
-        <tbody id="resultBox">
-            <?php
-            $sql = "SELECT * FROM shomarefaktor WHERE $date ORDER BY shomare DESC";
-            $result = mysqli_query(dbconnect(), $sql);
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $shomare = $row['shomare'];
-                    $kharidar = $row['kharidar'];
-                    $user = $row['user'];
-            ?>
-                    <tr>
+    <div id="resultBox">
+        <table class="customer-list jadval-shomare">
+            <tr>
+                <th>شماره فاکتور</th>
+                <th>خریدار</th>
+                <th>کاربر</th>
+                <th>ویرایش</th>
+            </tr>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM shomarefaktor WHERE $date ORDER BY shomare DESC";
+                $result = mysqli_query(dbconnect(), $sql);
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $shomare = $row['shomare'];
+                        $kharidar = $row['kharidar'];
+                        $user = $row['user'];
+                ?>
+                        <tr>
 
-                        <td>
-                            <div class="jadval-shomare-blue"><?php echo $shomare ?></div>
-                        </td>
-                        <td>
-                            <div class="jadval-shomare-kharidar"><?php echo $kharidar ?></div>
-                        </td>
-                        <td><img class="user-img" src="../userimg/<?php echo $user ?>.jpg" /></td>
+                            <td>
+                                <div class="jadval-shomare-blue"><?php echo $shomare ?></div>
+                            </td>
+                            <td>
+                                <div class="jadval-shomare-kharidar"><?php echo $kharidar ?></div>
+                            </td>
+                            <td><img class="user-img" src="../userimg/<?php echo $user ?>.jpg" /></td>
 
-                        <td><a id="<?php echo $row["id"] ?>" class="edit-shomare-faktor-btn">ویرایش<i class="fas fa-edit"></i></a></td>
+                            <td><a id="<?php echo $row["id"] ?>" class="edit-shomare-faktor-btn">ویرایش<i class="fas fa-edit"></i></a></td>
 
-                    </tr>
-            <?php
+                        </tr>
+                <?php
 
+                    }
                 }
-            }
 
-            ?>
-        </tbody>
-    </table>
-
+                ?>
+            </tbody>
+        </table>
+    </div>
 </div>
 <div id="myModal" class="modal">
     <div class="modal-content">
