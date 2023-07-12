eeac21f75d49925128428e0e6ac925dfc555c3ed
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-07-12 09:34:56 +0330

    Add a method to copy text to clipboard

--- tvAjax.php
+++ tvAjax.php
@@ -7,6 +7,24 @@ if (isset($_POST['user'])) {
 } else {
     $user  = getinternal($_SESSION["id"]);
 }
+function getFirstLetters($string)
+{
+    // Trim the string and remove special characters
+    $string = trim(preg_replace('/[^a-zA-Z0-9\sآ-ی]/u', '', $string));
+
+    $words = preg_split('/\s+/u', $string);
+    $firstLetters = '';
+
+    if (count($words) === 1) {
+        $firstLetters = mb_substr($words[0], 0, 2);
+    } else {
+        foreach ($words as $word) {
+            $firstLetters .= mb_substr($word, 0, 1) . ' ';
+        }
+    }
+
+    return trim($firstLetters);
+}
 ?>
 <script>
     function closeFullscreen() {
